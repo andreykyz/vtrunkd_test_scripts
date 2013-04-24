2dc44e4169acac157000add4441576b3ec3db632
Kyznecov Andrey <andreykyz@gmail.com>
2013-04-24 12:37:40 +0400

    post query fixes

--- webGui/setSSID.sh
+++ webGui/setSSID.sh
@@ -5,8 +5,9 @@ CHANNEL=0 # 0 is auto
 WIFI_ENABLE=1
 COUNTRY=RU
 HIDE=0
+WIFI_RESTART=1
 wget --header='X-Requested-With:XMLHttpRequest' --header='Accept-Language:ru;q=0.7,en;q=0.3' \
 --header='Content-Type:application/x-www-form-urlencoded; charset=UTF-8' \
 --header="Referer:http://$MODEM_IP/html/wlanbasicsettings.html" \
---post-data "<?xml version="1.0" encoding=UTF-8 ?><request><WifiSsid>$SSID</WifiSsid><WifiChannel>$CHANNEL</WifiChannel><WifiHide>$HIDE</WifiHide><WifiCountry>$COUNTRY</WifiCountry><WifiMode>b/g/n</WifiMode><WifiRate>0</WifiRate><WifiTxPwrPcnt>100</WifiTxPwrPcnt><WifiMaxAssoc>5</WifiMaxAssoc><WifiEnable>$WIFI_ENABLE</WifiEnable><WifiFrgThrshld>2346</WifiFrgThrshld><WifiRtsThrshld>2347</WifiRtsThrshld><WifiDtmIntvl>2</WifiDtmIntvl><WifiBcnIntvl>1</WifiBcnIntvl><WifiWme>0</WifiWme><WifiPamode>1</WifiPamode><WifiIsolate>1</WifiIsolate><WifiProtectionmode>0</WifiProtectionmode><Wifioffenable>0</Wifioffenable><Wifiofftime>1800</Wifiofftime><WifiRestart>0</WifiRestart></request>" \
-http://192.168.1.1/api/wlan/basic-settings -O /dev/null -q
+--post-data "<?xml version="1.0" encoding=UTF-8 ?><request><WifiSsid>$SSID</WifiSsid><WifiChannel>$CHANNEL</WifiChannel><WifiHide>$HIDE</WifiHide><WifiCountry>$COUNTRY</WifiCountry><WifiMode>b/g/n</WifiMode><WifiRate>0</WifiRate><WifiTxPwrPcnt>100</WifiTxPwrPcnt><WifiMaxAssoc>5</WifiMaxAssoc><WifiEnable>$WIFI_ENABLE</WifiEnable><WifiFrgThrshld>2346</WifiFrgThrshld><WifiRtsThrshld>2347</WifiRtsThrshld><WifiDtmIntvl>2</WifiDtmIntvl><WifiBcnIntvl>1</WifiBcnIntvl><WifiWme>0</WifiWme><WifiPamode>1</WifiPamode><WifiIsolate>1</WifiIsolate><WifiProtectionmode>0</WifiProtectionmode><Wifioffenable>0</Wifioffenable><Wifiofftime>1800</Wifiofftime><WifiRestart>$WIFI_RESTART</WifiRestart></request>" \
+http://$MODEM_IP/api/wlan/basic-settings -O /dev/null -q
--- webGui/setWifiPass.sh
+++ webGui/setWifiPass.sh
@@ -7,4 +7,4 @@ wget --header='X-Requested-With:XMLHttpRequest' --header='Accept-Language:ru;q=0
 --header='Content-Type:application/x-www-form-urlencoded; charset=UTF-8' \
 --header="Referer:http://$MODEM_IP/html/wlanbasicsettings.html" \
 --post-data "<?xml version="1.0" encoding=UTF-8 ?><request><WifiAuthmode>$AUTH_MODE</WifiAuthmode><WifiBasicencryptionmodes>NONE</WifiBasicencryptionmodes><WifiWpaencryptionmodes>$CRYPT_MODE</WifiWpaencryptionmodes><WifiWepKey1>60606</WifiWepKey1><WifiWepKey2>12345</WifiWepKey2><WifiWepKey3>12345</WifiWepKey3><WifiWepKey4>12345</WifiWepKey4><WifiWepKeyIndex>1</WifiWepKeyIndex><WifiWpapsk>$PSK_PASSWORD</WifiWpapsk><WifiWpsenbl>1</WifiWpsenbl><WifiWpscfg>1</WifiWpscfg><WifiRestart>1</WifiRestart></request>" \
-http://192.168.1.1/api/wlan/security-settings -O /dev/null -q
+http://$MODEM_IP/api/wlan/security-settings -O /dev/null -q
