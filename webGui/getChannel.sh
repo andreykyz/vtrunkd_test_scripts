e098ea7a7d1752a181067e095d61088b01858fc8
Kyznecov Andrey <andreykyz@gmail.com>
2013-04-24 12:49:30 +0400

    add auto mode - getChannel.sh

--- webGui/getChannel.sh
+++ webGui/getChannel.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 MODEM_IP='192.168.1.1'
-wget --header="X-Requested-With:XMLHttpRequest" --header="Accept-Language:ru;q=0.7,en;q=0.3" http://$MODEM_IP/api/wlan/basic-settings -O - -q | xml2 | grep WifiChannel | awk -F= '{ print$2 }'
+CHANNEL=`wget --header="X-Requested-With:XMLHttpRequest" --header="Accept-Language:ru;q=0.7,en;q=0.3" http://$MODEM_IP/api/wlan/basic-settings -O - -q | xml2 | grep WifiChannel | awk -F= '{ print$2 }'`
+if [ $CHANNEL = '0' ] ;
+then
+  CHANNEL=auto
+fi
+echo $CHANNEL
